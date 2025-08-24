4dfb66e3e259de48bf818b3a852d2d8a464f8ad0
Meet-s15 <meetsangani150807@gmail.com>
2025-08-24 19:31:58 +0530

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,41 @@
+import { Toaster } from "@/components/ui/toaster";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Index from "./pages/Index";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import MenClothing from "./pages/MenClothing";
+import WomenClothing from "./pages/WomenClothing";
+
+import ProductDetail from "./pages/ProductDetail";
+import NotFound from "./pages/NotFound";
+import { CartProvider } from "./contexts/CartContext";
+
+const queryClient = new QueryClient();
+
+const App = () => (
+  <QueryClientProvider client={queryClient}>
+    <CartProvider>
+      <TooltipProvider>
+        <Toaster />
+        <Sonner />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Index />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/men" element={<MenClothing />} />
+            <Route path="/women" element={<WomenClothing />} />
+
+            <Route path="/product/:id" element={<ProductDetail />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </BrowserRouter>
+      </TooltipProvider>
+    </CartProvider>
+  </QueryClientProvider>
+);
+
+export default App;
